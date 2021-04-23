4e149a5d0224c95cd0a3fb333a4efa14e9d4fae0
Лескина Наталья Александровна <nleskina@walker.vtb24.ru>
2021-04-23 09:29:07 +0300

    lab3

--- src/ru/lab3/ClassExample.java
+++
@@ -1,7 +0,0 @@
-package ru.lab3;
-
-public class ClassExample {
-    public static void main(String[] args) {
-        System.out.println("Hello Natalya!");
-    }
-}
---
+++ src/ru/lab3/Main.java
@@ -0,0 +1,72 @@
+package ru.lab3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numbers!");
+        String userInput = "";
+        int firstNumber = 0;
+        int secondNumber = 0;
+        while (!"q".equals(userInput)) {
+            try {
+                if (scanner.hasNextInt()) {
+                    firstNumber = scanner.nextInt();
+                }
+                if (scanner.hasNextLine()) {
+                    userInput = scanner.nextLine();
+                }
+                if (scanner.hasNextInt()) {
+                    secondNumber = scanner.nextInt();
+                }
+                double result;
+                switch (userInput) {
+                    case "+":
+                        result = plus(firstNumber, secondNumber);
+                        break;
+                    case "-":
+                        result = minus(firstNumber, secondNumber);
+                        break;
+                    case "*":
+                        result = ymn(firstNumber, secondNumber);
+                        break;
+                    case "/":
+                        if (secondNumber == 0) {
+                            continue;
+                        }
+                        result = del(firstNumber, secondNumber);
+                        break;
+                    case "!":
+                        result = fact(firstNumber);
+                        break;
+                    default:
+                        result = 0;
+                        break;
+                }
+                System.out.println(result);
+            } catch (InputMismatchException ex) {
+                System.out.println("Wrong input data!");
+                continue;
+            }
+        }
+    }
+    private static int plus(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+    private static int minus(int numberOne, int numberTwo) {
+        return numberOne - numberTwo; }
+    private static int ymn(int numberOne, int numberTwo) {
+        return numberOne * numberTwo;
+    }
+    private static int del(int numberOne, int numberTwo) {
+        return numberOne / numberTwo;
+    }
+    private static int fact(int numberOne){
+        int res = 1;
+        for (int i = 1; i < numberOne; i++) {
+            res *= i;
+        }
+        return res; }
+}

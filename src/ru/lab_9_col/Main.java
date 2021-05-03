d8bdb81ec8b198bf0ca87211d805045ea51d1914
nleskina <NLeskina@inno.tech>
2021-05-03 21:35:56 +0300

    Добавлены работы 7-10

---
+++ src/ru/lab_9_col/Main.java
@@ -0,0 +1,41 @@
+package ru.lab_9_col;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeMap<String, TreeMap<String, Integer>> clients = new TreeMap<String, TreeMap <String, Integer>>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите через пробел Покупателя, Товар и Количество:");
+        while(sc.hasNext()) {
+            String s = sc.nextLine();
+            if(s.equals("exit")) {
+                break;
+            }
+            String[] parts = s.split(" ");
+            String name = parts[0];
+            String productName = parts[1];
+            Integer count = Integer.parseInt(parts[2]);
+            if (!clients.containsKey(name))
+                clients.put(name, new TreeMap <String, Integer>());
+            TreeMap <String, Integer> temp = clients.get(name);
+            if (!temp.containsKey(productName))
+                temp.put(productName,0);
+            Integer oldCount = temp.get(productName);
+            temp.put(productName, oldCount + count);
+        }
+        for(Map.Entry<String, TreeMap <String, Integer>> entry : clients.entrySet()) {
+            String key = entry.getKey();
+            TreeMap <String, Integer> value = entry.getValue();
+            //Выводим имя покупателя
+            System.out.println(key + ":");
+
+            for(Map.Entry<String,Integer> product : value.entrySet()) {
+                String keyProduct = product.getKey(); Integer valueProduct = product.getValue();
+                System.out.println(keyProduct + " " + valueProduct);
+            }
+        }
+    }
+}
